334f095435771906d8c7b0666a1a569469f4301f
Anthony Sturdy <anthonysturdy123@gmail.com>
2019-03-12 19:47:25 +0000

    Converted BMP loader from C to C++

--- FOGGS_OpenGL_FreeGLUT/FOGGS_OpenGL_FreeGLUT/BMPLoader.cpp
+++ FOGGS_OpenGL_FreeGLUT/FOGGS_OpenGL_FreeGLUT/BMPLoader.cpp
@@ -1,48 +1,51 @@
 #include "BMPLoader.h"
 
 namespace BMPLoader {
-	struct bitmap {
+	struct BitMap {
 		unsigned int width, height;
 		unsigned char* pixels;
 	};
 
-	bitmap* bmp;
+	BitMap* bmp;
 
 	char* BMPLoader::LoadBitMap(const char* fileName) {
+		//Reset bmp from previous texture
 		if (bmp && bmp->pixels) {
 			bmp->width = 0;
 			bmp->height = 0;
-			free(bmp->pixels);
-			free(bmp);
-			bmp = NULL;
+
+			delete bmp->pixels;
+			delete bmp;
+
+			bmp = nullptr;
 		} else {
-			bmp = NULL;
+			bmp = nullptr;
 		}
 
-		FILE* f = fopen(fileName, "rb");
-
-		if (f) {
-			bmp = (bitmap*)malloc(sizeof(bitmap));
+		std::ifstream inFile(fileName, std::ifstream::binary);
+		if (inFile.good()) {
+			bmp = new BitMap();
 			bmp->width = 0;
 			bmp->height = 0;
-			bmp->pixels = NULL;
+			bmp->pixels = nullptr;
 
 			unsigned char info[54] = { 0 };
-			fread(info, sizeof(unsigned char), 54, f);
+			inFile.read((char*)&info[0], 54);
 
+			//Ask about "*(unsigned int*)&", without the *'s and & symbol it returns 0.
 			bmp->width = *(unsigned int*)&info[18];
 			bmp->height = *(unsigned int*)&info[22];
 
 			unsigned int size = ((((bmp->width * bmp->height) + 31) & ~31) / 8) * bmp->height;
-			bmp->pixels = (unsigned char*)malloc(size);
-			fread(bmp->pixels, sizeof(unsigned char), size, f);
-			fclose(f);
+			bmp->pixels = new unsigned char[size];
+			inFile.read((char*)bmp->pixels, size);
+			inFile.close();
 
-			//Changes RGB to BGR
-			for (int i = 0; i < size; i += 3) {
-				unsigned char tmp = bmp->pixels[i];
+			//Change BGR to RGB
+			for (int i = 0; i < size / 3; i++) {
+				unsigned char temp = bmp->pixels[i];
 				bmp->pixels[i] = bmp->pixels[i + 2];
-				bmp->pixels[i + 2] = tmp;
+				bmp->pixels[1 + 2] = temp;
 			}
 
 			return (char*)bmp->pixels;
--- FOGGS_OpenGL_FreeGLUT/FOGGS_OpenGL_FreeGLUT/BMPLoader.h
+++ FOGGS_OpenGL_FreeGLUT/FOGGS_OpenGL_FreeGLUT/BMPLoader.h
@@ -2,9 +2,9 @@
 #pragma warning(disable:4996)
 #include<fstream>
 #include<iostream>
-#include<cstdio>
 
 // 24 bit BMP reader. Code from: https://www.daniweb.com/programming/software-development/threads/482083/reading-a-24-bit-bmp-image-file
+// Converted from C to C++
 
 namespace BMPLoader {
 	char* LoadBitMap(const char* fileName);
